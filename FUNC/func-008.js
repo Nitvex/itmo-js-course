b57fa34f27b9188b7270856e2a099f980f72245e
Nitvex <nikita_vozisov@epam.com>
2020-02-08 21:32:57 +0300

    [fix] func-008

--- FUNC/func-008.js
+++ FUNC/func-008.js
@@ -1,12 +1,14 @@
-// board https://kodaktor.ru/func_9414c - doesn't work after clicking 'view'???
+// board https://kodaktor.ru/func_505fd
+var x;
 (async () => {
   const response = await fetch("https://3336.kodaktor.ru/funcpsst", {
     method: "POST"
   });
   const { g } = await response.json();
   const funcs = g.map(f => new Function(f));
-  const f0Args = Array.from(document.querySelectorAll(".f0")).map(arg => arg.value)
-  const f1Args = Array.from(document.querySelectorAll(".f1")).map(arg => arg.value)
-  console.log(funcs[0].apply(f0Args));
-  console.log(funcs[1].apply(f1Args));
+  console.log(funcs[0]);
+  x = Array.from(document.querySelectorAll(".f0")).map(arg => arg.value);
+  console.log(funcs[0].apply(x));
+  x = Array.from(document.querySelectorAll(".f1")).map(arg => arg.value);
+  console.log(funcs[1].apply(x));
 })();
