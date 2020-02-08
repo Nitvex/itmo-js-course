ee747c8a182a5c3ec83b63d194e251dd6077a2c7
Nitvex <nikita_vozisov@epam.com>
2020-02-08 19:21:37 +0300

    [add] func-007

--- FUNC/func-007.js
+++ FUNC/func-007.js
@@ -1,3 +1,4 @@
+//board https://kodaktor.ru/func_b3392
 const sec = a => 1 + a;
 const add = (a, b) => (b === 0 ? a : sec(add(a, b - 1)));
 const mpy = (a, b) => (b === 1 ? a : add(a, mpy(a, b - 1)));
