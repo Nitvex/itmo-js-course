3ddd3c7197ff0b68c7d6462fca7c75ed71718536
Nitvex <nikita_vozisov@epam.com>
2020-02-08 17:41:50 +0300

    [add] func-001

---
+++ FUNC/func-001.js
@@ -0,0 +1,7 @@
+function rgbColor(r = 255, g = 255, b = 255) {
+  return `rgb(${r}, ${g}, ${b})`;
+}
+console.log(rgbColor(1, 2, 3)); // rgb(1, 2, 3)
+console.log(rgbColor(1, 2)); // rgb(1, 2, 255)
+console.log(rgbColor(1)); // rgb(1, 255, 255)
+console.log(rgbColor()); // rgb(255, 255, 255)
