f0622a478ab95c4210ae360be354d3bf976b9685
Nitvex <nikita_vozisov@epam.com>
2020-02-08 19:07:42 +0300

    [add] func-007

---
+++ FUNC/func-007.js
@@ -0,0 +1,6 @@
+const sec = a => 1 + a;
+const add = (a, b) => (b === 0 ? a : sec(add(a, b - 1)));
+const mpy = (a, b) => (b === 1 ? a : add(a, mpy(a, b - 1)));
+const pwr = (a, b) => (b === 1 ? a : mpy(a, pwr(a, b - 1)));
+
+console.log(pwr(2, 13)); // pwr(11, 5) or pwr(2,14) causes Maximum call stask exceeded error
