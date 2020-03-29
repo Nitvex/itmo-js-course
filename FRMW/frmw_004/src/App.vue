92f9651ca29f1b31df64f6e1826d42e38be69795
Nitvex <nikita_vozisov@epam.com>
2020-03-29 15:26:40 +0300

    [add] frmw_004

---
+++ FRMW/frmw_004/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <input placeholder="base" v-model="base" />
+    <input placeholder="power" v-model="power" />
+
+    <list-item
+      v-for="index in powerArray"
+      :key="index"
+      :power="Number(index)"
+      :base="Number(base)"
+    />
+  </div>
+</template>
+
+<script>
+import ListItem from "./components/ListItem.vue";
+
+export default {
+  name: "App",
+  components: {
+    ListItem
+  },
+  data: () => ({
+    power: 0,
+    base: 0
+  }),
+  computed: {
+    powerArray() {
+      return Array.from(Array(Number(this.power)).keys());
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
